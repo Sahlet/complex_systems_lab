7385dc9ffeb4e61f52b4bcbeec83629edc96c321
Unknown <sahlet@yandex.ru>
2017-10-09 23:34:35 +0300

    commit

--- irrigation_canal_model.js
+++ irrigation_canal_model.js
@@ -1,57 +1,62 @@
 ;
+'use strict' //strict mode
 //-------------------------------------------------------
 
-function nodeFields() {
-	var node = {
-		paret : null,
-		children : [], //list of nodes
+function Model(model, parent_ref, content_ref) {
+	if ('object' !== typeof model) {
+		try {
+			model = JSON.parse(model);
+		} catch (e) {
+			console.log(e);
+		}
+	}
 
-		height : 2.0,
-		length : 100.0,
-		width : 3.0,
+	if (('object' === typeof model) && model) {
+		for (prop_name in model) {
+			this[prop_name] = model[prop_name];
+		}
 
-		enginePower : 1.0,
+		this.children = [];
 
-		waterLevel : 0.0, //coefficient of height
-		waterLevelLimits : {
-			lower : 4.0,
-			higher : 9.0
-		},
+		if ("children" in model) {
+			for (prop_name in model.children) {
+				this.children[prop_name] = new Model(model.children[prop_name], this);
+			}
+		}
+	} else {
+		this.height = 2.0;
+		this.length = 100.0;
+		this.width = 3.0;
 
-		content : null //some data for model user
-	};
+		this.enginePower = 1.0;
 
-	return node;
+		this.waterLevel = 0.0; //coefficient of height
+		this.waterLevelLimits = {
+			lower : 0.4,
+			higher : 0.9
+		};
+
+		this.children = [], //list of nodes
+	}
+
+	if (parent_ref) {
+		this.paret = parent_ref;
+	} else {
+		this.paret = null;
+	}
+
+	this.content = content_ref; //some data for model user
+
+	Object.defineProperty(this, "paret", { enumerable : false });
+	Object.defineProperty(this, "content", { enumerable : false });
 };
 
-var nodeFunctions = {
-
-	think : function(node, deltaTime /*in ms*/) {
-		//...
-		for (var i = Things.length - 1; i >= 0; i--) {
-			children[i].think(deltaTime);
-		}
-	},
-
-	parse : function (str) {
-		return JSON.parse(str);
-	},
-
-	stringify : function (node) {
-		return JSON.stringify(node);
+Model.prototype.think = function (deltaTime /*in ms*/) {
+	//...
+	for (var i = Things.length - 1; i >= 0; i--) {
+		children[i].think(deltaTime);
 	}
 };
-
-function modelFields() {
-	var model = {
-		mainNode : null,
-		think : function(deltaTime) {
-			nodeFunctions.think(mainNode, deltaTime);
-		}
-	};
-
-	return model;
-}
 
 //-------------------------------------------------------
 ;
