be93bd281871041a80bceb2bec4b62fd9cf28640
Unknown <sahlet@yandex.ru>
2017-10-12 09:00:20 +0300

    commit

--- irrigation_canal_model.js
+++ irrigation_canal_model.js
@@ -2,6 +2,22 @@
 'use strict' //strict mode
 //-------------------------------------------------------
 
+var default_model = {
+	climb : 1, // in m
+	height : 2, // in m
+	length : 100, // in m
+	width : 3, // in m
+	enginePower : 1, // in W
+	leak : 0.01, // in m/sec
+	waterLevelLimits : {
+		lower : 0.4,
+		higher : 0.9
+	},
+	waterLevel : 0, //coefficient of height
+
+	children : [], //list of nodes
+};
+
 function Model(model, parent_ref, content_ref) {
 	if ('object' !== typeof model) {
 		try {
@@ -13,37 +29,43 @@ function Model(model, parent_ref, content_ref) {
 
 //------------------------------
 
-	if (('object' === typeof model) && model) {
-		for (prop_name in model) {
-			this[prop_name] = model[prop_name];
+	if (!model || ("object" !== typeof model)) {
+		model = default_model;
+	}
+
+	for (prop_name in model) {
+		this[prop_name] = model[prop_name];
+	}
+
+	this.children = [];
+
+	if ("children" in model) {
+		for (prop_name in model.children) {
+			this.children[prop_name] = new Model(model.children[prop_name], this);
 		}
-
-		this.children = [];
-
-		if ("children" in model) {
-			for (prop_name in model.children) {
-				this.children[prop_name] = new Model(model.children[prop_name], this);
-			}
-		}
-	} else {
-		this.height = 2.0;
-		this.length = 100.0;
-		this.width = 3.0;
-		this.enginePower = 1.0;
-		this.waterLevelLimits = {
-			lower : 0.4,
-			higher : 0.9
-		};
-		this.waterLevel = 0.0; //coefficient of height
-
-		this.children = []; //list of nodes
 	}
 
 //------------------------------
 
-	Object.defineProperty(this, "engineWorks", {
+	Object.defineProperty(this, "engineWorksOnChange", {
+		value : new ObservableSubject(),
+		writable : false,
+		enumerable : false,
+		configurable : false
+	});
+	Object.defineProperty(this, "_engineWorks_", {
 		value : false,
 		writable : true,
+		enumerable : false,
+		configurable : false
+	});
+	Object.defineProperty(this, "engineWorks", {
+		get : function() { return this._engineWorks_; },
+		set : function(value) {
+			if (this._engineWorks_ === value) { return; }
+			this._engineWorks_ = value;
+			this.engineWorksOnChange.notify(this._engineWorks_, this);
+		},
 		enumerable : false,
 		configurable : false
 	});
@@ -60,8 +82,8 @@ function Model(model, parent_ref, content_ref) {
 		get : function() { return this._parent_; },
 		set : function(value) {
 			if (this.parent === value) { return; }
-			if (value !== null && classof(value) != "Model") {
-				console.warn("classof(value) != \"Model\"");
+			if (value !== null && "Model" != classof(value)) {
+				console.warn("\"Model\" != classof(value)");
 			}
 			this.parent = value;
 			this.updateEngineWorks();
@@ -93,13 +115,12 @@ function Model(model, parent_ref, content_ref) {
 	Object.defineProperty(this, "waterLevel", {
 		get : function() { return this._waterLevel_; },
 		set : function(value) {
+			if (this._waterLevel_ === clamp_value) { return; }
+
 			var clamp_value = Math.max(0, Math.min(1, value));
-			console.log("in set 1");
-			if (this._waterLevel_ !== clamp_value) {
-				console.log("in set 2");
-				this._waterLevel_ = clamp_value;
-				this.waterLevelOnChange.notify(this._waterLevel_, this);
-			}
+			this._waterLevel_ = clamp_value;
+			this.waterLevelOnChange.notify(this._waterLevel_, this);
+			updateEngineWorks();
 		},
 		enumerable : true,
 		configurable : false
@@ -107,11 +128,30 @@ function Model(model, parent_ref, content_ref) {
 
 //------------------------------
 
-	this.content = content_ref; //some data for model user
-	Object.defineProperty(this, "content", { enumerable : false });
+	Object.defineProperty(this, "content", { //some data for model user
+		value : content_ref,
+		writable : true,
+		enumerable : false,
+		configurable : false
+	});
 	
 //------------------------------
 
+	Object.defineProperty(this, "counting_waterLevel", { //to not call waterLevelOnChange during counting
+		value : this.waterLevel,
+		writable : true,
+		enumerable : false,
+		configurable : false
+	});
+
+//------------------------------
+
+};
+
+//------------------------------
+
+Model.prototype.getVolume = function () {
+	return this.height*this.length*this.width;
 };
 
 //------------------------------
@@ -123,11 +163,44 @@ Model.prototype.updateEngineWorks = function () {
 //------------------------------
 
 Model.prototype.think = function (deltaTime /*in ms*/) {
-	//...
+	this.counting_waterLevel = this.waterLevel;
+	var deltaTime_in_sec = deltaTime / 1000;
+
+	if (this.engineWorks) {
+		var g = 10; // acceleration
+		var h = 
+			(this.parent ? this.parent.height * (1 - this.parent.counting_waterLevel) : 0) +
+			this.climb + cur_waterLevel * height;
+
+		//enginePower = mass*g*h/deltaTime_in_sec
+
+		var mass = (this.enginePower * deltaTime_in_sec) / (g * h);
+
+		if (this.parent) {
+			var parent_mass = this.parent.counting_waterLevel * this.parent.getVolume();
+			mass = Math.min(mass, parent_mass);
+		}
+
+		if (mass > 0) {
+			this.counting_waterLevel += mass / this.getVolume();
+			this.parent.counting_waterLevel -= mass / this.parent.getVolume();
+			if (this.parent.counting_waterLevel < 0) {
+				this.parent.counting_waterLevel = 0;
+			}
+		}
+	}
+
 	children.forEach(function(value) {
 		value.think(deltaTime);
 	});
-	updateEngineWorks();
+
+	this.counting_waterLevel -= this.leak * deltaTime_in_sec;
+
+	if (this.counting_waterLevel < 0) {
+		this.counting_waterLevel = 0;
+	}
+
+	this.waterLevel = this.counting_waterLevel;
 };
 
 //------------------------------
